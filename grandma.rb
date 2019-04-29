0af7af31f9db8d04f86a5776fc664e73cf74e4d1
Irma J Mejia <ijanettmejia@outlook.com>
2019-04-29 03:24:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,7 @@ def speak_to_grandma(phrase = "Hi Nana, how are you?")
   if phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "HI!"
+    puts "HUH?! SPEAK UP, SONNY!"
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     return "NO, NOT SINCE 1938!"
